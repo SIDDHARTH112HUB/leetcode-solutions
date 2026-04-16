8e1ab63550eca2abf1ae5f64e07214506dc85579
Siddharth Maurya <sid@Siddharths-MacBook-Air.local>
2026-04-17 00:58:56 +0530

    LeetCode 35

---
+++ DSA/35. Search Insert Position.java
@@ -0,0 +1,27 @@
+//Find the position in array where
+//target value is present or if not present 
+// then the position where it can be inserted in sorted order
+class Solution {
+    public int searchInsert(int[] nums, int target) {
+        int n =nums.length;
+        int start =0;
+        int end = n-1;
+        int res=0;
+        if(nums[0]>target)
+        return 0;
+        while(start<=end){
+            int mid =start+(end-start)/2;
+            if(nums[mid]==target)
+                return mid;
+            else if(nums[mid]<target)
+            {
+                res =mid;
+                start=mid+1;
+            }
+            else
+                end =mid-1;
+        }
+        
+        return res+1;
+    }
+}
