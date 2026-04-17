9159f8e54551592b94efd33c3f0c9903a06ab87e
Siddharth Maurya <sid@Siddharths-MacBook-Air.local>
2026-04-18 01:42:01 +0530

    Peak of mountain array

---
+++ DSA/895_Peak_Mountain_Array.java
@@ -0,0 +1,23 @@
+package DSA;
+
+class Solution {
+    public int peakIndexInMountainArray(int[] arr) {
+        int l= 1;
+        int r=arr.length-2;
+
+        while(l<=r){
+            int mid=l+(r-l)/2;
+
+            if(arr[mid]>arr[mid-1] && arr[mid]>arr[mid+1]){
+                return mid;
+            }
+            else if( arr[mid]>arr[mid-1]){
+                l=mid+1;
+            }
+            else{
+                r=mid-1;
+            }
+        }
+        return -1;
+    }
+}
