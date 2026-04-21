3be5dcac1c53ad961f875c4c830f2641645c340d
Siddharth Maurya <67329960+SIDDHARTH112HUB@users.noreply.github.com>
2026-04-22 03:01:28 +0530

    Time: 6 ms (86.77%) | Memory: 46.1 MB (70.57%) - LeetSync

--- DSA/300-longest-increasing-subsequence/longest-increasing-subsequence.java
+++ DSA/300-longest-increasing-subsequence/longest-increasing-subsequence.java
@@ -1,51 +1,37 @@
-M-1 DP O(n^2) time complexity
-
-class Solution {
-    public int lengthOfLIS(int[] nums) {
-        int[] dp = new int[nums.length];
-        Arrays.fill(dp, 1);
-        int max = 1;
-
-        for (int a = 1; a < nums.length; a++) {
-            for (int b = a-1; b >= 0; b--) {
-                if (nums[b] < nums[a]) {
-                    dp[a] = Math.max(dp[a], dp[b] + 1);
-                    max = Math.max(max, dp[a]);
-                }
-                if (dp[a] > b) break;
-            }
-        }
-        return max;
-    }
-}
-
-
-M2- NlogN time complexity
-
 class Solution {
     public int lengthOfLIS(int[] nums) {
         if (nums == null || nums.length == 0) return 0;
         
         List<Integer> tails = new ArrayList<>();
-        
-        for (int num : nums) {
-            // Collections.binarySearch returns the index of num
-            // or (-(insertion point) - 1) if not present
-            int index = Collections.binarySearch(tails, num);
-            
-            // If not found, convert negative result to the actual insertion point
-            if (index < 0) {
-                index = -(index + 1);
+        tails.add(nums[0]);
+        int ans=1;
+        for (int i=1;i<nums.length;i++){
+            int num = nums[i]; 
+            if(num>tails.get(tails.size()-1))
+            {    
+                tails.add(num);
+                ans++;
             }
             
-            if (index == tails.size()) {
-                tails.add(num);
-            } else {
+            else{
+                int index = findLowerBound(tails,tails.size(), num);
                 tails.set(index, num);
             }
         }
         
-        return tails.size();
+        return ans;
+    }
+    private int findLowerBound(List<Integer> arr, int high, int target) {
+        int low = 0;
+        while (low < high) {
+            int mid = (low + high) >>> 1;
+            if (arr.get(mid) < target) { 
+                low = mid + 1;
+            } else {
+                high = mid;
+            }
+        }
+        return low;
     }
 }
 
