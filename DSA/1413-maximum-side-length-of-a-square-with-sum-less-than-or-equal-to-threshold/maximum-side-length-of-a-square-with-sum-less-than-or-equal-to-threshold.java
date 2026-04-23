15f2f21ea13de97a7b90890b8d431b4c8c0a3e01
Siddharth Maurya <67329960+SIDDHARTH112HUB@users.noreply.github.com>
2026-04-24 02:05:05 +0530

    Time: 8 ms (86.82%) | Memory: 63.4 MB (23.00%) - LeetSync

--- DSA/1413-maximum-side-length-of-a-square-with-sum-less-than-or-equal-to-threshold/maximum-side-length-of-a-square-with-sum-less-than-or-equal-to-threshold.java
+++ DSA/1413-maximum-side-length-of-a-square-with-sum-less-than-or-equal-to-threshold/maximum-side-length-of-a-square-with-sum-less-than-or-equal-to-threshold.java
@@ -1,43 +1,48 @@
 class Solution {
     public int maxSideLength(int[][] mat, int threshold) {
-        int m = mat.length;
-        int n = mat[0].length;
-        int ans = 0;
+        int m = mat.length, n = mat[0].length;
 
         // Build prefix sum matrix
-        int[][] preSum = new int[m][n];
-        preSum[0][0] = mat[0][0];
-        for (int i = 1; i < m; i++) preSum[i][0] = preSum[i-1][0] + mat[i][0];
-        for (int j = 1; j < n; j++) preSum[0][j] = preSum[0][j-1] + mat[0][j];
-        for (int i = 1; i < m; i++) {
-            for (int j = 1; j < n; j++) {
-                preSum[i][j] = mat[i][j] 
-                             + preSum[i-1][j] 
-                             + preSum[i][j-1] 
+        int[][] preSum = new int[m+1][n+1]; // +1 for easier boundaries
+        for (int i = 1; i <= m; i++) {
+            for (int j = 1; j <= n; j++) {
+                preSum[i][j] = mat[i-1][j-1]
+                             + preSum[i-1][j]
+                             + preSum[i][j-1]
                              - preSum[i-1][j-1];
             }
         }
 
-        // Try every possible top-left corner (i, j)
-        for (int i = 0; i < m; i++) {
-            for (int j = 0; j < n; j++) {
-                for (int t = 1; i + t <= m && j + t <= n; t++) {
-                    int r1 = i, c1 = j;
-                    int r2 = i + t - 1, c2 = j + t - 1;
+        int ans = 0;
+        int low = 1, high = Math.min(m, n);
 
-                    int sum = preSum[r2][c2];
-                    if (r1 > 0) sum -= preSum[r1-1][c2];
-                    if (c1 > 0) sum -= preSum[r2][c1-1];
-                    if (r1 > 0 && c1 > 0) sum += preSum[r1-1][c1-1];
-
-                    if (sum <= threshold) {
-                        ans = Math.max(ans, t);
-                    } else {
-                        break; // larger squares will only increase sum
-                    }
-                }
+        // Binary search on side length
+        while (low <= high) {
+            int mid = (low + high) / 2;
+            if (existsSquare(preSum, mid, threshold)) {
+                ans = mid;       // mid works, try bigger
+                low = mid + 1;
+            } else {
+                high = mid - 1;  // mid too large, try smaller
             }
         }
         return ans;
+    }
+
+    // Check if any square of side 'len' has sum <= threshold
+    private boolean existsSquare(int[][] preSum, int len, int threshold) {
+        int m = preSum.length - 1;
+        int n = preSum[0].length - 1;
+
+        for (int i = len; i <= m; i++) {
+            for (int j = len; j <= n; j++) {
+                int sum = preSum[i][j]
+                        - preSum[i-len][j]
+                        - preSum[i][j-len]
+                        + preSum[i-len][j-len];
+                if (sum <= threshold) return true;
+            }
+        }
+        return false;
     }
 }
