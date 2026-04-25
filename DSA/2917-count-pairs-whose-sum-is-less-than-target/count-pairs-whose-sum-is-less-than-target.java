7f05a07a9aa7910bff2132800e68a33e62a0e14c
Siddharth Maurya <67329960+SIDDHARTH112HUB@users.noreply.github.com>
2026-04-26 02:55:47 +0530

    Time: 3 ms (46.78%) | Memory: 44.2 MB (31.35%) - LeetSync

--- DSA/2917-count-pairs-whose-sum-is-less-than-target/count-pairs-whose-sum-is-less-than-target.java
+++ DSA/2917-count-pairs-whose-sum-is-less-than-target/count-pairs-whose-sum-is-less-than-target.java
@@ -1,10 +1,33 @@
+// class Solution {
+//     public int countPairs(List<Integer> nums, int target) {
+//         int ans=0;
+//         for(int i=0;i<nums.size();i++){
+//             for( int j=i+1;j<nums.size();j++){
+//                 if(nums.get(i)+nums.get(j)<target)
+//                 ans++;
+//             }
+//         }
+//         return ans;
+//     }
+// }
+
+import java.util.*;
+
+//m2- two pointer
+
 class Solution {
     public int countPairs(List<Integer> nums, int target) {
-        int ans=0;
-        for(int i=0;i<nums.size();i++){
-            for( int j=i+1;j<nums.size();j++){
-                if(nums.get(i)+nums.get(j)<target)
-                ans++;
+        Collections.sort(nums); // O(n log n)
+        int ans = 0;
+        int left = 0, right = nums.size() - 1;
+
+        while (left < right) {
+            if (nums.get(left) + nums.get(right) < target) {
+                // All pairs (left, left+1 ... right) are valid
+                ans += (right - left);
+                left++;
+            } else {
+                right--;
             }
         }
         return ans;
