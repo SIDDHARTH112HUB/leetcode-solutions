fa3244d80f8ffed628dcd84413ecddaa32d17d2a
Siddharth Maurya <67329960+SIDDHARTH112HUB@users.noreply.github.com>
2026-05-01 02:05:59 +0530

    Time: 117 ms (14.34%) | Memory: 91.7 MB (60.09%) - LeetSync

--- DSA/1549-longest-continuous-subarray-with-absolute-diff-less-than-or-equal-to-limit/longest-continuous-subarray-with-absolute-diff-less-than-or-equal-to-limit.java
+++ DSA/1549-longest-continuous-subarray-with-absolute-diff-less-than-or-equal-to-limit/longest-continuous-subarray-with-absolute-diff-less-than-or-equal-to-limit.java
@@ -1,5 +1,5 @@
 class Solution {
-    public int longestSubarray(int[] nums, int limit) {
+    public int longestSubarray1(int[] nums, int limit) {
         TreeMap<Integer,Integer> map = new TreeMap<>();
         int i=0,j=0,ans=0;
         while(j<nums.length){
@@ -12,6 +12,33 @@ class Solution {
             }
             ans = Math.max(ans,j-i+1);
             j++;
+        }
+        return ans;
+    }
+    public int longestSubarray(int[] nums, int limit) {
+        TreeMap<Integer, Integer> map = new TreeMap<>();
+        int i = 0, j = 0, ans = 0;
+
+        while (j < nums.length) {
+            // 1. Add the NEW element at index j to the map
+            int incoming = nums[j];
+            map.put(incoming, map.getOrDefault(incoming, 0) + 1);
+
+            // 2. If the limit is exceeded, shrink the window from the left (i)
+            while (map.lastKey() - map.firstKey() > limit) {
+                int outgoing = nums[i];
+                int count = map.get(outgoing);
+                if (count == 1) {
+                    map.remove(outgoing);
+                } else {
+                    map.put(outgoing, count - 1);
+                }
+                i++; // Shrink window
+            }
+
+            // 3. Update the maximum length found so far
+            ans = Math.max(ans, j - i + 1);
+            j++; // Expand window
         }
         return ans;
     }
