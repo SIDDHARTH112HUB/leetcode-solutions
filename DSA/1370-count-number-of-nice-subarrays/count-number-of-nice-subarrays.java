62ddca6fcf249a7ee3eaf6fad993f063bb473478
Siddharth Maurya <67329960+SIDDHARTH112HUB@users.noreply.github.com>
2026-05-02 01:19:40 +0530

    Time: 13 ms (70.37%) | Memory: 59.8 MB (27.20%) - LeetSync

--- DSA/1370-count-number-of-nice-subarrays/count-number-of-nice-subarrays.java
+++ DSA/1370-count-number-of-nice-subarrays/count-number-of-nice-subarrays.java
@@ -1,5 +1,23 @@
 class Solution {
-    public int numberOfSubarrays(int[] nums, int k) {
+    public int numberOfSubarraysM1UsingQueue(int[] nums, int k) {
+        Queue<Integer> q = new LinkedList<>();
+        int i=0,j=0,ans=0;
+        while(j<nums.length){
+            if(nums[j]%2==1)
+            q.add(j);
+            while(q.size()>k){
+                i=q.poll()+1;
+                
+            }
+            if(q.size()==k){
+                ans+=q.peek()-i+1;
+            }
+            j++;
+
+        }
+        return ans;
+    }
+    public int numberOfSubarraysUsingArrays(int[] nums, int k) {
         int count=0;
         List<Integer> q = new ArrayList<>();
         int i=0,j=0,ans=0;
@@ -19,6 +37,26 @@ class Solution {
             j++;
 
         }
+        return ans;
+    }
+    public int numberOfSubarrays(int[] nums, int k) {
+        return atMost(nums, k) - atMost(nums, k - 1);
+    }
+
+    private int atMost(int[] nums, int k) {
+        int i = 0, ans = 0;
+
+        for (int j = 0; j < nums.length; j++) {
+            if (nums[j] % 2 == 1) k--;
+
+            while (k < 0) {
+                if (nums[i] % 2 == 1) k++;
+                i++;
+            }
+
+            ans += j - i + 1;
+        }
+
         return ans;
     }
 }
