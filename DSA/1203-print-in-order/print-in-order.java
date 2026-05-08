07e599da2f112d764076ecfdef7fe12f30521e14
Siddharth Maurya <67329960+SIDDHARTH112HUB@users.noreply.github.com>
2026-05-09 01:40:44 +0530

    Time: 10 ms (92.86%) | Memory: 44.2 MB (18.49%) - LeetSync

--- DSA/1203-print-in-order/print-in-order.java
+++ DSA/1203-print-in-order/print-in-order.java
@@ -1,29 +1,53 @@
 
-class Foo {
-    private CountDownLatch latch1;
-    private CountDownLatch latch2;
+// class Foo {
+//     private CountDownLatch latch1;
+//     private CountDownLatch latch2;
 
-    public Foo() {
-        latch1 = new CountDownLatch(1); // waits for first()
-        latch2 = new CountDownLatch(1); // waits for second()
-    }
+//     public Foo() {
+//         latch1 = new CountDownLatch(1); // waits for first()
+//         latch2 = new CountDownLatch(1); // waits for second()
+//     }
+
+//     public void first(Runnable printFirst) throws InterruptedException {
+//         // printFirst.run() outputs "first"
+//         printFirst.run();
+//         latch1.countDown(); // signal that first() is done
+//     }
+
+//     public void second(Runnable printSecond) throws InterruptedException {
+//         latch1.await(); // wait until first() finishes
+//         // printSecond.run() outputs "second"
+//         printSecond.run();
+//         latch2.countDown(); // signal that second() is done
+//     }
+
+//     public void third(Runnable printThird) throws InterruptedException {
+//         latch2.await(); // wait until second() finishes
+//         // printThird.run() outputs "third"
+//         printThird.run();
+//     }
+// }
+import java.util.concurrent.Semaphore;
+
+class Foo {
+    private Semaphore sem1 = new Semaphore(0);
+    private Semaphore sem2 = new Semaphore(0);
+
+    public Foo() {}
 
     public void first(Runnable printFirst) throws InterruptedException {
-        // printFirst.run() outputs "first"
         printFirst.run();
-        latch1.countDown(); // signal that first() is done
+        sem1.release(); // allow second() to run
     }
 
     public void second(Runnable printSecond) throws InterruptedException {
-        latch1.await(); // wait until first() finishes
-        // printSecond.run() outputs "second"
+        sem1.acquire(); // wait for first()
         printSecond.run();
-        latch2.countDown(); // signal that second() is done
+        sem2.release(); // allow third() to run
     }
 
     public void third(Runnable printThird) throws InterruptedException {
-        latch2.await(); // wait until second() finishes
-        // printThird.run() outputs "third"
+        sem2.acquire(); // wait for second()
         printThird.run();
     }
 }
