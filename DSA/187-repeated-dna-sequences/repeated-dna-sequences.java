6ffd89ae66b504922cbce4a27601f11013f15b3e
Siddharth Maurya <67329960+SIDDHARTH112HUB@users.noreply.github.com>
2026-05-10 16:10:13 +0530

    Time: 18 ms (83.92%) | Memory: 51.8 MB (97.66%) - LeetSync

--- DSA/187-repeated-dna-sequences/repeated-dna-sequences.java
+++ DSA/187-repeated-dna-sequences/repeated-dna-sequences.java
@@ -1,5 +1,5 @@
 class Solution {
-    public List<String> findRepeatedDnaSequences(String s) {
+    public List<String> findRepeatedDnaSequences1(String s) {
         Set<String> seen=new HashSet<>();
         Set<String>repeated=new HashSet<>();
         for(int i=0;i<=s.length()-10;i++){
@@ -8,5 +8,28 @@ class Solution {
             else seen.add(cur);
         }
         return new ArrayList<>(repeated);
+    }
+    public List<String> findRepeatedDnaSequences(String s) {
+        Set<Integer> set = new HashSet<>();
+        int[] map = new int[26];
+        map['A' - 'A'] = 0;
+        map['C' - 'A'] = 1;
+        map['G' - 'A'] = 2;
+        map['T' - 'A'] = 3;
+        Set<String> tmp = new HashSet<>();
+
+        for(int i = 9; i < s.length(); i++) {
+            int mask = 0;
+            for(int j = i - 9; j <= i; j++) {
+                char c = s.charAt(j);
+                mask = mask << 2;
+                mask = mask | map[c - 'A'];
+                
+            }
+            if(!set.add(mask)) {
+                tmp.add(s.substring(i-9, i + 1));
+            }
+        }
+        return new ArrayList<>(tmp);
     }
 }
