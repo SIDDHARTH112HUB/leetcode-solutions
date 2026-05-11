c748c81f4623c7338b862ce00a3bdb984ab864b8
Siddharth Maurya <67329960+SIDDHARTH112HUB@users.noreply.github.com>
2026-05-12 01:59:48 +0530

    Time: 0 ms (100.00%) | Memory: 44 MB (30.97%) - LeetSync

--- DSA/1755-defuse-the-bomb/defuse-the-bomb.java
+++ DSA/1755-defuse-the-bomb/defuse-the-bomb.java
@@ -1,5 +1,5 @@
 class Solution {
-    public int[] decrypt(int[] nums, int k) {
+    public int[] decrypt1(int[] nums, int k) {
         int n = nums.length;
         int []ans = new int[n];
         if(k==0)
@@ -23,5 +23,27 @@ class Solution {
             }
         }
         return ans;
+    }
+
+    public int[] decrypt(int[] code, int k) {
+        int[] result = new int[code.length];
+        if (k == 0) return result;
+        // Define the initial window and initial sum
+        int start = 1, end = k, sum = 0;
+        // If k < 0, the starting point will be end of the array.
+        if (k < 0) {
+            start = code.length - Math.abs(k);
+            end = code.length - 1;
+        }
+        for (int i = start; i <= end; i++) sum += code[i];
+        // Scan through the code array as i moving to the right, update the window sum.
+        for (int i = 0; i < code.length; i++) {
+            result[i] = sum;
+            sum -= code[(start) % code.length];
+            sum += code[(end + 1) % code.length];
+            start++;
+            end++;
+        }
+        return result;
     }
 }
