d540823da98487d7745879860b4969dacbb1aa49
Siddharth Maurya <siddharth.maurya112@gmail.com>
2026-05-18 01:40:03 +0530

    top View

---
+++ DSA Standard Patterns/Tree/14_TopViewOfTree.java
@@ -0,0 +1,51 @@
+/*
+class Node {
+    int data;
+    Node left, right;
+
+    Node(int val) {
+        this.data = val;
+        this.left = null;
+        this.right = null;
+    }
+}
+*/
+import java.util.*;
+class Solution {
+    class Pair{
+        int key;
+        Node node;
+        public Pair(int k, Node n){
+            key= k;
+            node = n;
+        }
+    }
+    public ArrayList<Integer> topView(Node root) {
+        ArrayList<Integer> ans = new ArrayList<>();
+        TreeMap<Integer,Integer> mp = new TreeMap<>();
+        Queue<Pair> q = new LinkedList<>();
+        if(root==null)
+        return ans; 
+
+        q.add(new Pair(0,root));
+        int level =0;
+        while(!q.isEmpty()){
+            int n = q.size();
+            while(n-->0){
+                Pair t = q.poll();
+                int x = t.key;
+                mp.computeIfAbsent(x, k -> t.node.data);
+                if(t.node.left!=null){
+                    q.add(new Pair(x-1,t.node.left));
+                }                
+                if(t.node.right!=null){
+                    q.add(new Pair(x+1,t.node.right));
+                }
+            }
+        }
+        for(int a : mp.values()){
+            ans.add(a);
+        }
+        return ans;
+    }
+}
