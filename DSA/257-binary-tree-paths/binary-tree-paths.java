97502662c66eb329b42dfa3bc580bd6ae06d80ee
Siddharth Maurya <67329960+SIDDHARTH112HUB@users.noreply.github.com>
2026-05-19 01:35:42 +0530

    Time: 1 ms (100.00%) | Memory: 44.1 MB (78.20%) - LeetSync

--- DSA/257-binary-tree-paths/binary-tree-paths.java
+++ DSA/257-binary-tree-paths/binary-tree-paths.java
@@ -14,7 +14,7 @@
  * }
  */
 class Solution {
-    public List<String> binaryTreePaths(TreeNode root) {
+    public List<String> binaryTreePaths1(TreeNode root) {
         ArrayList <String> ans = new ArrayList<>();
         if(root==null) return ans;
         String a = "";
@@ -33,5 +33,27 @@ class Solution {
         {ans.add(a);
         return ;
         }
+    }
+    public List<String> binaryTreePaths(TreeNode root) {
+         List<String> res = new ArrayList<>();
+        StringBuilder sb = new StringBuilder();
+        helper(res, root, sb);
+        return res;
+        
+    }
+     private void helper(List<String> res, TreeNode root, StringBuilder sb) {
+        if(root == null) {
+            return;
+        }
+        int len = sb.length();
+        sb.append(root.val);
+        if(root.left == null && root.right == null) {
+            res.add(sb.toString());
+        } else {
+            sb.append("->");
+            helper(res, root.left, sb);
+            helper(res, root.right, sb);
+        }
+        sb.setLength(len);
     }
 }
