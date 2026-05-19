bd12eec5c34240755b60d5f21d20cacea77ec6a5
Siddharth Maurya <67329960+SIDDHARTH112HUB@users.noreply.github.com>
2026-05-20 03:03:37 +0530

    Time: 12 ms (83.86%) | Memory: 44 MB (73.60%) - LeetSync

---
+++ DSA/893-all-nodes-distance-k-in-binary-tree/all-nodes-distance-k-in-binary-tree.java
@@ -0,0 +1,68 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+class Solution {
+    class Pair {
+        TreeNode par;  // actual tree node
+        TreeNode node;  // actual tree node
+        public Pair(TreeNode root, TreeNode baap) {
+            node = root;
+            par = baap;
+        }
+    }
+    public List<Integer> distanceK(TreeNode root, TreeNode target, int k) {
+        Queue<TreeNode> q = new LinkedList<>();
+        Map<TreeNode,TreeNode> mp = new HashMap<>();
+        HashSet<TreeNode> visited = new HashSet<>(); 
+        q.add(root);
+
+        while(!q.isEmpty()){
+            int n=q.size();
+            while(n-->0){
+                TreeNode t = q.poll();
+                if(t.left!=null) {
+                    mp.put(t.left,t);
+                    q.add(t.left);
+                }
+                if(t.right!=null){
+                    mp.put(t.right,t);
+                    q.add(t.right);
+                } 
+            }
+        }
+        q.add(target);
+
+        visited.add(target);
+        while(k>0 && !q.isEmpty()){
+            int n=q.size();
+            while(n-->0){
+                TreeNode t = q.poll();
+                if(t.left!=null && !visited.contains(t.left)) {
+                    q.add(t.left);
+                    visited.add(t.left);
+                }
+                if(t.right!=null && !visited.contains(t.right)) {
+                    q.add(t.right);
+                    visited.add(t.right);
+                }
+                TreeNode par= mp.get(t);
+                if(par!=null&& !visited.contains(par)){
+                    q.add(par);
+                    visited.add(par);
+                } 
+            }
+            k--;
+        }
+        List<Integer> ans = new ArrayList<>();
+        while(!q.isEmpty()){
+            ans.add(q.poll().val);
+        }
+        return ans;
+    }
+}
