8cc2fc5d49a3a34ffaf90cdae0a897bf170bf494
Siddharth Maurya <67329960+SIDDHARTH112HUB@users.noreply.github.com>
2026-05-23 00:16:42 +0530

    Time: 0 ms (100.00%) | Memory: 49.6 MB (12.68%) - LeetSync

--- DSA/222-count-complete-tree-nodes/count-complete-tree-nodes.java
+++ DSA/222-count-complete-tree-nodes/count-complete-tree-nodes.java
@@ -15,7 +15,7 @@
  */
 class Solution {
     int n=0;
-    public int countNodes(TreeNode root) {
+    public int countNodes1(TreeNode root) {
         if(root==null)
         return n;
         countHelper(root);
@@ -27,5 +27,35 @@ class Solution {
         n++;
         countHelper(root.left);
         countHelper(root.right);
+    }
+    public int countNodes(TreeNode root) {
+        if(root==null)
+        return 0;
+
+        int l = leftHeight(root);
+        int r = rightHeight(root);
+
+        if(l==r) return ((2<<l) -1);
+
+        return countNodes(root.left)+countNodes(root.right)+1;
+    }
+
+    int leftHeight(TreeNode root) {
+        int n=0;
+        while(root.left!=null){
+            root=root.left;
+            n++;
+        }
+
+        return n;
+    }
+    int rightHeight(TreeNode root) {
+        int n=0;
+        while(root.right!=null){
+            root=root.right;
+            n++;
+        }
+
+        return n;
     }
 }
