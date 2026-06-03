a5608b0174a46916762932a54add82ce9b743981
Siddharth Maurya <67329960+SIDDHARTH112HUB@users.noreply.github.com>
2026-06-04 01:42:57 +0530

    Time: 17 ms (25.52%) | Memory: 47.2 MB (94.63%) - LeetSync

---
+++ DSA/297-serialize-and-deserialize-binary-tree/serialize-and-deserialize-binary-tree.java
@@ -0,0 +1,60 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+public class Codec {
+
+    // Encodes a tree to a single string.
+    public String serialize(TreeNode root) {
+        if(root==null)
+        return "";
+        StringBuilder sb = new StringBuilder();
+        Queue<TreeNode> q = new LinkedList<>();
+        q.add(root);
+        while(!q.isEmpty()){
+            TreeNode temp = q.poll();
+            if(temp==null){
+                sb.append("n ");
+                continue;
+            }
+            else{
+                sb.append(temp.val+" ");
+            }
+            q.add(temp.left);
+            q.add(temp.right);
+        } 
+        return sb.toString();
+    }
+
+    // Decodes your encoded data to tree.
+    public TreeNode deserialize(String data) {
+        if(data=="")
+        return null;
+        String []v = data.split(" ");
+        TreeNode root = new TreeNode(Integer.parseInt(v[0]));
+        Queue<TreeNode> q = new LinkedList<>();
+        q.add(root);
+        for (int i=1;i<v.length;i++){
+            TreeNode temp = q.poll();
+            if(!v[i].equals("n")){
+                temp.left = new TreeNode(Integer.parseInt(v[i]));
+                q.add(temp.left);
+            }
+            if(!v[++i].equals("n")){
+                temp.right = new TreeNode(Integer.parseInt(v[i]));
+                q.add(temp.right);
+            }
+        }
+        return root;
+    }
+}
+
+// Your Codec object will be instantiated and called as such:
+// Codec ser = new Codec();
+// Codec deser = new Codec();
+// TreeNode ans = deser.deserialize(ser.serialize(root));
