b5279cd77a97e95984cffbb0da55ef7ed82a7613
Siddharth Maurya <67329960+SIDDHARTH112HUB@users.noreply.github.com>
2026-06-07 01:29:40 +0530

    Time: 0 ms (100.00%) | Memory: 44.1 MB (32.05%) - LeetSync

--- DSA/114-flatten-binary-tree-to-linked-list/flatten-binary-tree-to-linked-list.java
+++ DSA/114-flatten-binary-tree-to-linked-list/flatten-binary-tree-to-linked-list.java
@@ -15,7 +15,7 @@
  */
 class Solution {
     TreeNode prev = null;
-    public void flatten(TreeNode root) {
+    public void flatten1(TreeNode root) {
         if(root==null)
         return;
 
@@ -25,5 +25,20 @@ class Solution {
         root.right=prev;
         root.left = null;
         prev = root;
+    }
+     public void flatten(TreeNode root) {
+        TreeNode curr = root;
+        while (curr != null) {
+            if (curr.left != null) {
+                TreeNode prev = curr.left;
+                while (prev.right != null) {
+                    prev = prev.right;
+                }
+                prev.right = curr.right;
+                curr.right = curr.left;
+                curr.left = null;
+            }
+            curr = curr.right;
+        }
     }
 }
