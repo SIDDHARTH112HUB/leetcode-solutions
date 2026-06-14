205f776f744cdbf73995905e1900b59475b6d5fc
Siddharth Maurya <67329960+SIDDHARTH112HUB@users.noreply.github.com>
2026-06-14 16:17:21 +0530

    Time: 3 ms (76.85%) | Memory: 47.4 MB (36.24%) - LeetSync

--- DSA/653-two-sum-iv-input-is-a-bst/two-sum-iv-input-is-a-bst.java
+++ DSA/653-two-sum-iv-input-is-a-bst/two-sum-iv-input-is-a-bst.java
@@ -13,18 +13,57 @@
  *     }
  * }
  */
+ class BSTIterator {
+    private Stack<TreeNode> st = new Stack<>();
+    private boolean reverse;
+
+    // reverse = false → inorder (smallest to largest)
+    // reverse = true  → reverse inorder (largest to smallest)
+    public BSTIterator(TreeNode root, boolean reverse) {
+        this.reverse = reverse;
+        pushAll(root);
+    }
+
+    public boolean hasNext() {
+        return !st.isEmpty();
+    }
+
+    public int next() {
+        TreeNode node = st.pop();
+        if (!reverse) {
+            pushAll(node.right);
+        } else {
+            pushAll(node.left);
+        }
+        return node.val;
+    }
+
+    private void pushAll(TreeNode node) {
+        while (node != null) {
+            st.push(node);
+            node = reverse ? node.right : node.left;
+        }
+    }
+}
 class Solution {
     public boolean findTarget(TreeNode root, int k) {
-        Set<Integer> s = new HashSet<>();
-        Queue<TreeNode> q = new LinkedList<>();
-        q.add(root);
-        while(!q.isEmpty()){
-            TreeNode t = q.poll();
-            if(s.contains(k-t.val))
-            return true;
-            s.add(t.val);
-            if(t.left!=null)q.add(t.left);
-            if(t.right!=null)q.add(t.right);
+        if (root == null) return false;
+
+        BSTIterator l = new BSTIterator(root, false);  // smallest
+        BSTIterator r = new BSTIterator(root, true);   // largest
+
+        int i = l.next();
+        int j = r.next();
+
+        while (i < j) {
+            if (i + j == k) return true;
+            else if (i + j < k) {
+                if (l.hasNext()) i = l.next();
+                else return false;
+            } else {
+                if (r.hasNext()) j = r.next();
+                else return false;
+            }
         }
         return false;
     }
