cb1fec4239a1193e2a557fa6b41f7b631c0c1d6c
Siddharth Maurya <67329960+SIDDHARTH112HUB@users.noreply.github.com>
2026-06-19 15:10:56 +0530

    Time: 1 ms (100.00%) | Memory: 42.5 MB (92.41%) - LeetSync

---
+++ DSA/4319-sum-of-compatible-numbers-in-range-i/sum-of-compatible-numbers-in-range-i.java
@@ -0,0 +1,11 @@
+class Solution {
+    public int sumOfGoodIntegers(int n, int k) {
+        int ans=0;
+        for(int i=Math.max(1, n - k);i<=n+k;i++){
+            if((i&n)==0){
+                ans+=i;
+            }
+        }
+        return ans;
+    }
+}
