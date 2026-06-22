bf190614d7b126dbdc5af8b26541ea0745eafe53
Siddharth Maurya <67329960+SIDDHARTH112HUB@users.noreply.github.com>
2026-06-23 02:12:14 +0530

    Time: 3 ms (87.52%) | Memory: 43.5 MB (25.80%) - LeetSync

--- DSA/20-valid-parentheses/valid-parentheses.java
+++ DSA/20-valid-parentheses/valid-parentheses.java
@@ -1,34 +1,20 @@
-import java.util.Stack;
-
 class Solution {
-    private boolean check(char start, char end) {
-        if (start == '(' && end == ')') return true;
-        if (start == '{' && end == '}') return true;
-        if (start == '[' && end == ']') return true;
-        return false;
-    }
-
-    private boolean isClose(char s) {
-        return (s == ')' || s == ']' || s == '}');
-    }
-
-    public boolean isValid(String str) {
-        if (str == null || str.isEmpty()) return true;
-
-        Stack<Character> stack = new Stack<>();
-        stack.push(str.charAt(0));
-
-        for (int i = 1; i < str.length(); i++) {
-            char t = str.charAt(i);
-            if (isClose(t)) {
-                if (stack.isEmpty()) return false;
-                if (!check(stack.peek(), t)) return false;
-                else stack.pop();
-            } else {
-                stack.push(t);
+    public boolean isValid(String s) {
+        Stack<Character> st= new Stack<>();
+        for(char c:s.toCharArray()){
+            if( c=='(' || c=='[' || c=='{'){
+                st.push(c);
             }
-        }
-
-        return stack.isEmpty();
+            else{
+                if(st.size()<=0)
+                return false;
+                char b = st.pop();
+                if((c==')'&& b!='(') || (c==']'&& b!='[') || (c=='}'&& b!='{'))
+                return false;
+            }
+        } 
+        if(st.size()>0)
+        return false;
+        return true;
     }
 }
