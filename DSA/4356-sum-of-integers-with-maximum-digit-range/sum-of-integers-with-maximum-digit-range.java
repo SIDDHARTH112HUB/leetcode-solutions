b6464bb7048a68f9e764cad80c97688fcae3cda5
Siddharth Maurya <67329960+SIDDHARTH112HUB@users.noreply.github.com>
2026-07-05 23:55:08 +0530

    Time: 1 ms (100.00%) | Memory: 45.2 MB (66.72%) - LeetSync

--- DSA/4356-sum-of-integers-with-maximum-digit-range/sum-of-integers-with-maximum-digit-range.java
+++ DSA/4356-sum-of-integers-with-maximum-digit-range/sum-of-integers-with-maximum-digit-range.java
@@ -1,22 +1,33 @@
 class Solution {
     public int maxDigitRange(int[] nums) {
-        int ans =0;
-        int currMax = 0;
-        for(int i:nums){
-            String s = Integer.toString(i);;
-            char[] charArray = s.toCharArray();
-
-            // Sort the character array
-            Arrays.sort(charArray);
-            int m = charArray[charArray.length-1]-charArray[0] ;
-            if(m>currMax){
-                currMax = m;
-                ans=i;
+        int n = nums.length;
+        int maxRange = 0;
+        int range = 0;
+        int[] individualRanges = new int[n];
+        
+        for (int i = 0; i < n; i++) {
+            int num = nums[i];
+            int max = Integer.MIN_VALUE;
+            int min = Integer.MAX_VALUE;
+            if (num == 0) {
+                max = 0;
+                min = 0;
+            } 
+            while (num > 0) {
+                max = Math.max(max, num % 10);
+                min = Math.min(min, num % 10);
+                num /= 10;
             }
-            else if(m==currMax){
-                ans+=i;
+            range = max - min;
+            individualRanges[i] = range;
+            maxRange = Math.max(maxRange, range);
+        }
+        int totalSum = 0;
+        for (int i = 0; i < n; i++) {
+            if (individualRanges[i] == maxRange) {
+                totalSum += nums[i];
             }
         }
-        return ans;
+        return totalSum;
     }
 }
