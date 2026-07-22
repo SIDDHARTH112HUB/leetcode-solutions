c1b090fc25125107a88f776b6c9393e1020c4146
Siddharth Maurya <67329960+SIDDHARTH112HUB@users.noreply.github.com>
2026-07-22 22:59:51 +0530

    Time: 0 ms (100.00%) | Memory: 46.3 MB (64.46%) - LeetSync

--- DSA/777-toeplitz-matrix/toeplitz-matrix.java
+++ DSA/777-toeplitz-matrix/toeplitz-matrix.java
@@ -1,5 +1,5 @@
 class Solution {
-    public boolean isToeplitzMatrix(int[][] mat) {
+    public boolean isToeplitzMatrix1(int[][] mat) {
         int m = mat.length;
         int n = mat[0].length;
         for(int i=0;i<n;i++){
@@ -23,5 +23,18 @@ class Solution {
             }
         }
         return true;
+    }
+    public boolean isToeplitzMatrix(int[][] matrix) {
+        
+
+        for (int i=1; i<matrix.length; i++){
+            for (int j=1; j < matrix[i].length; j++){
+                if (matrix[i][j]!=matrix[i-1][j-1])
+                return false;
+
+            }
+        }
+        return true;
+        
     }
 }
