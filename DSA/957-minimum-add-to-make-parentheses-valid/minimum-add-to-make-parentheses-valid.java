73f7b89b774e5f9a2e797b10e86df9c5f3a922bd
Siddharth Maurya <67329960+SIDDHARTH112HUB@users.noreply.github.com>
2026-08-08 01:58:52 +0530

    Time: 0 ms (100.00%) | Memory: 42.3 MB (98.96%) - LeetSync

--- DSA/957-minimum-add-to-make-parentheses-valid/minimum-add-to-make-parentheses-valid.java
+++ DSA/957-minimum-add-to-make-parentheses-valid/minimum-add-to-make-parentheses-valid.java
@@ -1,19 +1,33 @@
 class Solution {
     public int minAddToMakeValid(String s) {
-        Stack<Character> st = new Stack<>();
-        for(char c:s.toCharArray()){
-            if(c=='('){
-                st.push(c);
-            }
-            else {
-                if(!st.isEmpty()&&st.peek()=='('){
-                    st.pop();
-                }
-                else{
-                    st.push(c);
-                }
-            }
+        // Stack<Character> st = new Stack<>();
+        // for(char c:s.toCharArray()){
+        //     if(c=='('){
+        //         st.push(c);
+        //     }
+        //     else {
+        //         if(!st.isEmpty()&&st.peek()=='('){
+        //             st.pop();
+        //         }
+        //         else{
+        //             st.push(c);
+        //         }
+        //     }
+        // }
+        // return st.size();
+        int l = 0;
+        int r = 0;
+
+        for (final char c : s.toCharArray())
+        if (c == '(') {
+            ++l;
+        } else {
+            if (l == 0)
+            ++r;
+            else
+            --l;
         }
-        return st.size();
+
+        return l + r;
     }
 }
