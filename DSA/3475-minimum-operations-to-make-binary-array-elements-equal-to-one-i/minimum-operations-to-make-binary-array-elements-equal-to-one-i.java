afa0230397be9eb3e22d129d12b55b37b2b4e6dc
Siddharth Maurya <67329960+SIDDHARTH112HUB@users.noreply.github.com>
2026-08-23 23:03:57 +0530

    Time: 6 ms (100.00%) | Memory: 102.2 MB (87.21%) - LeetSync

--- DSA/3475-minimum-operations-to-make-binary-array-elements-equal-to-one-i/minimum-operations-to-make-binary-array-elements-equal-to-one-i.java
+++ DSA/3475-minimum-operations-to-make-binary-array-elements-equal-to-one-i/minimum-operations-to-make-binary-array-elements-equal-to-one-i.java
@@ -1,31 +1,17 @@
 class Solution {
     public int minOperations(int[] nums) {
-        int c =0;
-        int ans =0;
-        boolean one = true;
-        int i;
-        for(i=0;i<nums.length-2;){
-            if(nums[i]==0){
-                ans++;
-                nums[i+1] = (nums[i+1]+1)%2;
-                nums[i+2] = (nums[i+2]+1)%2;
-                
-                i++;
-                if(nums[i]==1){
-                    i++;
-                    if(nums[i]==1){
-                        i++;
-                    }
-                }
-            }
-            else{
-                i++;
-            }
-        }
+        int count = 0;
         int n = nums.length;
-        if(i==nums.length || (nums[n-1]==1 && nums[n-2]==1)){
-            return ans;
+
+        for (int i = 0; i < n - 2; i++) {
+            if (nums[i] == 0) {
+                nums[i] ^= 1;
+                nums[i + 1] ^= 1;
+                nums[i + 2] ^= 1;
+                count++;
+            }
         }
-        return -1;
+
+        return (nums[n - 2] == 1 && nums[n - 1] == 1) ? count : -1;
     }
 }
