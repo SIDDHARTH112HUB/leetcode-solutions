37351a6fa97d13c7fc3357fd8f39409af52ea2fe
Siddharth Maurya <67329960+SIDDHARTH112HUB@users.noreply.github.com>
2026-08-25 00:40:06 +0530

    Time: 50 ms (47.96%) | Memory: 52.6 MB (53.73%) - LeetSync

--- DSA/1582-design-browser-history/design-browser-history.java
+++ DSA/1582-design-browser-history/design-browser-history.java
@@ -1,36 +1,79 @@
 class BrowserHistory {
-    Stack<String> curr,fwd;
+    // Stack<String> curr,fwd;
+    // public BrowserHistory(String homepage) {
+    //     curr = new Stack<>();
+    //     fwd = new Stack<>();
+    //     curr.push(homepage);
+    // }
+    
+    // public void visit(String url) {
+    //     curr.push(url);
+    //     fwd.clear();         
+    // }
+    
+    // public String back(int steps) {
+    //     while(steps>0 && curr.size()>1){
+    //         steps--;
+    //         fwd.push(curr.pop());
+    //     }
+    //     if(!curr.isEmpty())
+    //     return curr.peek();
+
+    //     return null;
+    // }
+    
+    // public String forward(int steps) {
+    //     while(steps>0 && !fwd.isEmpty()){
+    //         steps--;
+    //         curr.push(fwd.pop());
+    //     }
+    //     if(!curr.isEmpty())
+    //     return curr.peek();
+
+    //     return null;
+    // }
+    class Node{
+        String data;
+        Node next;
+        Node prev;
+        Node(String data){
+            this.data=data;
+        }
+    }
+    Node head,tail,cur;
     public BrowserHistory(String homepage) {
-        curr = new Stack<>();
-        fwd = new Stack<>();
-        curr.push(homepage);
+        Node temp=new Node(homepage);
+        head=temp;
+        tail=temp;
+        cur=temp;
     }
     
     public void visit(String url) {
-        curr.push(url);
-        fwd.clear();         
+        cur.next=null;
+        Node temp=new Node(url);
+        cur.next=temp;
+        temp.prev=cur;
+        cur=temp;
+        tail=temp;
     }
     
     public String back(int steps) {
-        while(steps>0 && curr.size()>1){
+        Node temp=cur;
+        while(steps>0&&temp!=head){
+            temp=temp.prev;
             steps--;
-            fwd.push(curr.pop());
         }
-        if(!curr.isEmpty())
-        return curr.peek();
-
-        return null;
+        cur=temp;
+        return temp.data;
     }
-    
     public String forward(int steps) {
-        while(steps>0 && !fwd.isEmpty()){
-            steps--;
-            curr.push(fwd.pop());
-        }
-        if(!curr.isEmpty())
-        return curr.peek();
-
-        return null;
+     Node temp=cur;
+     while(steps>0&&temp.next!=null){
+        steps--;
+        temp=temp.next;
+     }   
+     cur=temp;
+     return temp.data;
     }
 }
 
