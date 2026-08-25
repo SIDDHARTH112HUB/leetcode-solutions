32d02c4d73846da9745d67a944b3db7a65335f8b
Siddharth Maurya <67329960+SIDDHARTH112HUB@users.noreply.github.com>
2026-08-26 01:30:56 +0530

    Time: 3 ms (97.73%) | Memory: 42.4 MB (62.73%) - LeetSync

--- DSA/2408-number-of-people-aware-of-a-secret/number-of-people-aware-of-a-secret.java
+++ DSA/2408-number-of-people-aware-of-a-secret/number-of-people-aware-of-a-secret.java
@@ -1,6 +1,6 @@
 class Solution {
     int mod = (int)(1e9 + 7);
-    public int peopleAwareOfSecret(int n, int delay, int forget) {
+    public int peopleAwareOfSecret1(int n, int delay, int forget) {
         long[] dp = new long[n + 1];  // dp[i] = number of people who learn the secret on day i
         dp[1] = 1;
 
@@ -31,4 +31,35 @@ class Solution {
         }
         return (int) ans;
     }
+    public int peopleAwareOfSecret(int n, int delay, int forget) {
+
+        if (n == 1)
+            return 1;
+        final int MOD = 1_000_000_007;
+
+        long[] dp = new long[n + 1];
+        dp[1] = 1;
+        long newPeople = 0;
+
+        for (int i = 2; i <= n; i++) {
+            int canShare = i - delay;
+            int canForget = i - forget;
+
+            if (canShare > 0)
+                newPeople = (newPeople + dp[canShare]) % MOD;
+            if (canForget > 0)
+                newPeople = (newPeople - dp[canForget] + MOD) % MOD;
+            
+            dp[i] = newPeople;
+        }
+
+        long result = 0;
+        int start = Math.max(1, (n - forget + 1));
+        for (int i = start; i <= n; i++)
+            result = (result + dp[i]) % MOD;
+        
+        return (int) result;
+        
+    }
+
 }
