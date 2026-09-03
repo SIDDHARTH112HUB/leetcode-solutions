9d5e8a1514aff9de858370827e266daa7b85336d
Siddharth Maurya <67329960+SIDDHARTH112HUB@users.noreply.github.com>
2026-09-04 01:37:03 +0530

    Time: 5 ms (55.02%) | Memory: 47.8 MB (51.48%) - LeetSync

--- DSA/658-find-k-closest-elements/find-k-closest-elements.java
+++ DSA/658-find-k-closest-elements/find-k-closest-elements.java
@@ -6,16 +6,14 @@ class Pair {
         num = n;
     }
 }
-
 class Solution {
-    public List<Integer> findClosestElements(int[] arr, int k, int x) {
+    public List<Integer> findClosestElements1(int[] arr, int k, int x) {
         // Max heap: farthest distances at the top. Ties broken by larger number at the top.
         PriorityQueue<Pair> pq = new PriorityQueue<>(
             Comparator.comparingInt((Pair p) -> p.key)
                       .thenComparingInt(p -> p.num)
                       .reversed()
         );
-        
         for (int i : arr) {
             pq.add(new Pair(Math.abs(i - x), i));
             // Just need 'if' since we add one at a time
@@ -23,14 +21,23 @@ class Solution {
                 pq.poll();
             }
         }
-        
         List<Integer> ans = new ArrayList<>();
         while (!pq.isEmpty()) {
             ans.add(pq.poll().num);
         }
-        
         // Problem requires the result to be sorted in ascending order
         Collections.sort(ans);
         return ans;
+    }
+        public List<Integer> findClosestElements(int[] A, int k, int x) {
+        int left = 0, right = A.length - k;
+        while (left < right) {
+            int mid = (left + right) / 2;
+            if (x - A[mid] > A[mid + k] - x)
+                left = mid + 1;
+            else
+                right = mid;
+        }
+        return Arrays.stream(A, left, left + k).boxed().collect(Collectors.toList());
     }
 }
