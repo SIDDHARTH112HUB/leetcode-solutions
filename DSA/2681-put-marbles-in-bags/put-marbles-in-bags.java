0e5463c2185d334df14bb333782ae7e06d2ab9c7
Siddharth Maurya <67329960+SIDDHARTH112HUB@users.noreply.github.com>
2026-09-11 01:10:08 +0530

    Time: 38 ms (53.59%) | Memory: 80.1 MB (47.51%) - LeetSync

--- DSA/2681-put-marbles-in-bags/put-marbles-in-bags.java
+++ DSA/2681-put-marbles-in-bags/put-marbles-in-bags.java
@@ -1,56 +1,34 @@
-import java.util.PriorityQueue;
-import java.util.Collections;
-import java.util.List;
-import java.util.ArrayList;
+import java.util.Arrays;
 
 class Solution {
-    public long putMarbles(int[] w, int k) {
-        // Edge case: if bags equal 1 or the number of elements, difference is 0
-        if (k == 1 || k == w.length) {
+    public long putMarbles(int[] weights, int k) {
+        int n = weights.length;
+        
+        // If we only have 1 bag, or if bags equal the number of marbles, 
+        // there is only one way to distribute them. Max - Min = 0.
+        if (k == 1 || k == n) {
             return 0;
         }
 
-        // We need k - 1 cuts
-        List<Integer> maxk = topKLarge_small(w, k - 1, false);
-        List<Integer> mink = topKLarge_small(w, k - 1, true);
-        
+        // Array to store the sum of adjacent pairs (the "cut" points)
+        int[] pairs = new int[n - 1];
+        for (int i = 0; i < n - 1; i++) {
+            pairs[i] = weights[i] + weights[i + 1];
+        }
+
+        // Sort the pairs to easily pick the smallest and largest
+        Arrays.sort(pairs);
+
         long minScore = 0;
         long maxScore = 0;
-        
-        // Sum up the elements using long to prevent integer overflow
-        for (int val : mink) {
-            minScore += val;
-        }
-        for (int val : maxk) {
-            maxScore += val;
-        }
-        
-        return maxScore - minScore;
-    }
-    
-    public List<Integer> topKLarge_small(int[] nums, int cuts, boolean minimum) {
-        PriorityQueue<Integer> pq = new PriorityQueue<>();
-        
-        // If we want the minimums, we use a Max-Heap to eject the largest elements
-        if (minimum) {
-            pq = new PriorityQueue<>(Collections.reverseOrder());
-        }
-        
-        for (int j = 1; j < nums.length; j++) {
-            pq.add(nums[j] + nums[j - 1]);
-            
-            // Use 'if' instead of 'while' since it only ever grows by 1 per iteration
-            if (pq.size() > cuts) {
-                pq.poll();
-            }
-        }
-        
-        List<Integer> list = new ArrayList<>();
 
-        while (!pq.isEmpty()) {
-            list.add(pq.poll());
+        // The first and last elements are always included in every configuration.
+        // We only need to sum the (k - 1) smallest and largest cut points.
+        for (int i = 0; i < k - 1; i++) {
+            minScore += pairs[i];
+            maxScore += pairs[n - 2 - i]; // n - 2 is the last index of the pairs array
         }
-        
-        return list;
+
+        return maxScore - minScore;
     }
 }
