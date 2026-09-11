f97a05631cc258e120dab48054cee03f069b0a6b
Siddharth Maurya <67329960+SIDDHARTH112HUB@users.noreply.github.com>
2026-09-12 01:46:48 +0530

    Time: 1 ms (100.00%) | Memory: 43.7 MB (97.26%) - LeetSync

--- DSA/3799-unique-3-digit-even-numbers/unique-3-digit-even-numbers.java
+++ DSA/3799-unique-3-digit-even-numbers/unique-3-digit-even-numbers.java
@@ -1,5 +1,5 @@
 class Solution {
-    public int totalNumbers(int[] digits) {
+    public int totalNumbers1(int[] digits) {
         Set<Integer> uniqueNumbers = new HashSet<>();
 
         // Generate all permutations of size 3
@@ -24,5 +24,35 @@ class Solution {
         return uniqueNumbers.size();
 
 
+    }
+    public int totalNumbers(int[] digits) {
+        int[] freq=new int[10];
+        for(int digit:digits){
+            freq[digit]++;
+        }
+        int count=0;
+        for(int i=1;i<=9;i++){
+            if(freq[i]==0){
+                continue;
+            }
+            freq[i]--;
+
+            for(int j=0;j<=9;j++){
+                if(freq[j]==0){
+                    continue;
+                }
+                freq[j]--;
+
+                for(int k=0;k<=8;k+=2){
+                    if(freq[k]>0){
+                        int number=i*100+j*10+k;
+                        count++;
+                    }
+                }
+                freq[j]++;
+            }
+            freq[i]++;
+        }
+        return count;
     }
 }
