e4d86313c36aac1eee6d9117e020a547ff758822
Siddharth Maurya <67329960+SIDDHARTH112HUB@users.noreply.github.com>
2026-09-23 00:39:32 +0530

    Time: 15 ms (59.74%) | Memory: 51.7 MB (19.72%) - LeetSync

---
+++ DSA/455-assign-cookies/assign-cookies.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int findContentChildren(int[] g, int[] s) {
+        Arrays.sort(g);
+        Arrays.sort(s);
+        int ans =0;
+        int i=0,j=0;
+        while(i<s.length && j<g.length){
+            if(s[i]>=g[j]){
+                ans++;
+                j++;
+            }
+            i++;
+        }
+        return ans;
+    }
+}
